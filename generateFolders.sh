21f5dd6dd78e4d1e7b9337ec623735bc5a1a8e55
Rodrigo Fraga <rfraga@thinkmax.com>
2023-05-26 07:06:22 -0400

    Remove rust from generate folders

--- generateFolders.sh
+++ generateFolders.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the array of languages
-languages=("c" "cpp" "c#" "python" "rust" "golang")
+languages=("c" "cpp" "c#" "python" "golang")
 
 # Iterate over each language
 for language in "${languages[@]}"; do
